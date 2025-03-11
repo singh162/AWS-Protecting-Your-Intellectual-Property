b13ea4de040aa25f1046b94637bb24bf1ffc08c7
Jasmeet Singh <jasmeet.singh@bytescare.com>
2025-03-11 10:33:54 +0000

    added navigation bar

---
+++ pages/Login/jsobjects/NavigateToExternalUrl/NavigateToExternalUrl.js
@@ -0,0 +1,9 @@
+export default {
+	navigateFunction () {
+		if(Select1.selectedOptionLabel === "EN"){
+			navigateTo('https://saipdashboard.bytes.care/app/aws-protecting-your-intellectual-property/login-6773c714c83f61054013bc37?branch=master');
+		}else{
+			navigateTo('https://saipdashboard.bytes.care/app/arabic-protecting-your-intellectual-property/login-67c7ee213124d45b75bb8eb5?branch=master');
+		}
+	}
+}
