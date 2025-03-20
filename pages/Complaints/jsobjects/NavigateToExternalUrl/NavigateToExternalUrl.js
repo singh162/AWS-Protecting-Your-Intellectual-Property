fbfa28ea8d7a01e68851f6a65f84732c6874f038
Jasmeet Singh <jasmeet.singh@bytescare.com>
2025-03-20 06:07:02 +0000

    added the language drop down

---
+++ pages/Complaints/jsobjects/NavigateToExternalUrl/NavigateToExternalUrl.js
@@ -0,0 +1,9 @@
+export default {
+	navigateFunction () {
+		if(Select1Copy1.selectedOptionLabel === "EN"){
+			navigateTo('https://saipdashboard.bytes.care/app/aws-protecting-your-intellectual-property/complaints-6773c714c83f61054013bc36?branch=master');
+		}else{
+			navigateTo('https://saipdashboard.bytes.care/app/arabic-protecting-your-intellectual-property/complaints-67da722e3124d45b75bb9d00?branch=master');
+		}
+	}
+}
---
+++ pages/Registeration/jsobjects/NavigateToExternalUrl/NavigateToExternalUrl.js
@@ -0,0 +1,9 @@
+export default {
+	navigateFunction () {
+		if(Select1Copy1.selectedOptionLabel === "EN"){
+			navigateTo('https://saipdashboard.bytes.care/app/aws-protecting-your-intellectual-property/registeration-6773c714c83f61054013bc33?branch=master');
+		}else{
+			navigateTo('https://saipdashboard.bytes.care/app/arabic-protecting-your-intellectual-property/registeration-67d66c6e3124d45b75bb979f?branch=master');
+		}
+	}
+}
